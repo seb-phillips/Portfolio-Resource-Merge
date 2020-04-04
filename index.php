e387e52ac8c3bce6ed71f29e78f54b61172d358a
Seb Phillips <sebphillips1@yahoo.co.uk>
2020-04-04 19:12:18 +0100

    first commit - merge1

--- index.php
+++ index.php
@@ -1,26 +1,62 @@
-<?php include_once("head.php");?>
-<title>Resources - Seb Phillips</title>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="rellax.css">
+</head>
 <body>
+<section class="section section-top">
+    <div class="content rellax" data-rellax-speed="5">
+        <h1>Seb Phillips Portfolio Homepage</h1>
+        <!-- <a href="#" class="btn btn-primary">Learn More</a> -->
+    </div>
+</section>
 
-<?php require_once("nav.php");?>
+<section class="section section-stream">
+    <img
+        class="play rellax"
+        src="https://i.ibb.co/yS3W4Lk/sideimg2.jpg"
+        alt=""
+        data-rellax-speed="-1" data-rellax-xs-speed="-5"
+    />
+    <div class="content rellax" data-rellax-speed="10">
+        <div>
+            <h2 class="secondary-text">About</h2>
+            <p>Student at Loughborough University studying Information Technology Management for Business, a 50:50 split of Computer Science & Business modules.</p>
+        </div>
+        <div>
+            <h2 class="secondary-text">Work</h2 class="secondary-text">
+            <p>Currently on placement at CGI, as a consultant. Building Web, Java and Android skills in free time.</p>
+        </div>
+    </div>
+</section>
 
-<div style="margin-left:25%;padding:1px 16px;height:1000px;">
-  <h1>Resources</h1><br>
-  <h2>Seb Phillips</h2><br>
-  <h3>03/20 | Java, Android, Chatbot Theory</h3><br>
-  <h3>Stored Notes</h3>
-  <p>Creation 16/03/2020<br>PHP, HTML, CSS, for Java</p>
-  <p>Creation 19/03/2020<br>Chatbots - NLP, Design</p>
-  <p>Creation 23/03/2020<br>Android - Fundamentals & Projects</p>
-  <p>Creation 23/03/2020<br>Added PDF Viewer Functionality</p>
+<section class="section section-grid">
+    <div class="rellax" data-rellax-speed="1" data-rellax-xs-speed="3">
+        <i class="fas fa-video fa-3x secondary-text"></i>
+        <h2>LINKS<span class="secondary-text dot">.</span></h2>
+        <a href="resourcesHome.php"><i>Click here to find my learning resources</i></a><br><br>
+        <a href="https://github.com/seb-phillips"><i>Click here for my GitHub</i></a><br><br>
+        <a href="https://www.linkedin.com/in/seb-phillips-967ab0172/"><i>Click here for my LinkedIn</i></a>
+    </div>
+    <div class="rellax" data-rellax-speed="4" data-rellax-xs-speed="3">
+        <i class="fas fa-users fa-3x secondary-text"></i>
+        <h2>SKILLS & TOOLS USED<span class="secondary-text dot">.</span></h2>
+        <p>Java, Android, PHP, Javascript & Frameworks. </p>
+    </div>
+    <div class="rellax" data-rellax-speed="7" data-rellax-xs-speed="3">
+        <i class="fas fa-book fa-3x secondary-text"></i>
+        <h2>WEB<span class="secondary-text dot">.</span></h2>
+        <p>This homepage was created using Rellax.js, alongisde HTML & CSS</p>
+    </div>
+</section>
 
+<footer class="footer">
+<ul>
+<li><a href="#">Seb Phillips</a></li>
+</ul>
 
-  <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
-  <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
-  
-</div>
-
-
-
-
-<?php include_once("foot.php")?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.js" integrity="sha256-dwdFP62+BD8iDd62omP3wP6Ew0YKO8O+Zgg+gwyPMoc=" crossorigin="anonymous"></script>
+<!-- Init relax -->
+<script src="js/rellax.js"></script>
+</body>
+</html>
---
+++ resourcesHome.php
@@ -0,0 +1,26 @@
+<?php include_once("head.php");?>
+<title>Resources - Seb Phillips</title>
+<body>
+
+<?php require_once("nav.php");?>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+  <h1>Resources</h1><br>
+  <h2>Seb Phillips</h2><br>
+  <h3>03/20 | Java, Android, Chatbot Theory</h3><br>
+  <h3>Stored Notes</h3>
+  <p>Creation 16/03/2020<br>PHP, HTML, CSS, for Java</p>
+  <p>Creation 19/03/2020<br>Chatbots - NLP, Design</p>
+  <p>Creation 23/03/2020<br>Android - Fundamentals & Projects</p>
+  <p>Creation 23/03/2020<br>Added PDF Viewer Functionality</p>
+
+
+  <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
+  <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
+  
+</div>
+
+
+
+
+<?php include_once("foot.php")?>
